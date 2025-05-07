e87fd2f80e25be29c3ce9a0697780f7d694aeb11
Motsek-shabba <motsekitshabalala501@gmail.com>
2025-05-07 15:06:52 +0200

    Deployment

--- aspnet-core/src/EventManagement.EntityFrameworkCore/Migrations/20250507130324_Deploy.Designer.cs
+++ aspnet-core/src/EventManagement.EntityFrameworkCore/Migrations/20250507130324_Deploy.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EventManagement.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EventManagement.Migrations
 {
     [DbContext(typeof(EventManagementDbContext))]
-    partial class EventManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250507130324_Deploy")]
+    partial class Deploy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
